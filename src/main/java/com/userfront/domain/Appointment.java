f128fd71312c0ae0a87ef179d6ac1c81e84d768f
franzparks <franzparks@gmail.com>
2016-11-03 07:51:07 -0700

    add fields to Appointment

--- src/main/java/com/userfront/domain/Appointment.java
+++ src/main/java/com/userfront/domain/Appointment.java
@@ -1,5 +1,15 @@
 package com.userfront.domain;
 
+import java.util.Date;
+
 public class Appointment {
+	
+	private Long id;
+	private Date date;
+	private String location;
+	private String description;
+	private boolean confirmed;
+	
+	private User user;
 
 }
