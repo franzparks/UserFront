bcb9ff8211eba4f64c913e6bdc3f52de7a50a828
franzparks <franzparks@gmail.com>
2016-11-03 07:56:25 -0700

    add fields to Recipient

--- src/main/java/com/userfront/domain/Recipient.java
+++ src/main/java/com/userfront/domain/Recipient.java
@@ -1,5 +1,17 @@
 package com.userfront.domain;
 
+
+
 public class Recipient {
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String accountNumber;
+	private String description;
+	
+	
+	private User user;
 
 }
