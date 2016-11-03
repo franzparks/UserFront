599021afd1f2026d554df4240ef62c3f6736554d
franzparks <franzparks@gmail.com>
2016-11-03 02:46:25 -0700

    create PrimaryTransaction domain class

---
+++ src/main/java/com/userfront/domain/PrimaryTransaction.java
@@ -0,0 +1,145 @@
+package com.userfront.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PrimaryTransaction {
+	
+	private Long id;
+	private Date date;
+	private String description;
+	private String type;
+	private String status;
+	private double amount;
+	private BigDecimal availableBalance;
+	private PrimaryAccount primaryAccount;
+	
+	public PrimaryTransaction() {}
+
+	public PrimaryTransaction(Date date, String description, String type, String status, double amount,
+			BigDecimal availableBalance, PrimaryAccount primaryAccount) {
+		super();
+		this.date = date;
+		this.description = description;
+		this.type = type;
+		this.status = status;
+		this.amount = amount;
+		this.availableBalance = availableBalance;
+		this.primaryAccount = primaryAccount;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @return the availableBalance
+	 */
+	public BigDecimal getAvailableBalance() {
+		return availableBalance;
+	}
+
+	/**
+	 * @return the primaryAccount
+	 */
+	public PrimaryAccount getPrimaryAccount() {
+		return primaryAccount;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	/**
+	 * @param availableBalance the availableBalance to set
+	 */
+	public void setAvailableBalance(BigDecimal availableBalance) {
+		this.availableBalance = availableBalance;
+	}
+
+	/**
+	 * @param primaryAccount the primaryAccount to set
+	 */
+	public void setPrimaryAccount(PrimaryAccount primaryAccount) {
+		this.primaryAccount = primaryAccount;
+	}
+	
+	
+
+}
