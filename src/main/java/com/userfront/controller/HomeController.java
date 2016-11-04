e3d5c4fdba43a724005db2cd2cb2211c2a21234f
franzparks <franzparks@gmail.com>
2016-11-04 13:06:40 -0700

    autowire userservice in homecontroller

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.userfront.controller;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.userfront.domain.User;
+import com.userfront.service.UserService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/")
 	public String home(){
