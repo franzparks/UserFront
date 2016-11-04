813400b6c11bd232b4c7ed5cb3c9cc01505e1120
franzparks <franzparks@gmail.com>
2016-11-04 11:28:53 -0700

    add methods to interface

--- src/main/java/com/userfront/Dao/UserDao.java
+++ src/main/java/com/userfront/Dao/UserDao.java
@@ -5,5 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import com.userfront.domain.User;
 
 public interface UserDao extends CrudRepository<User, Long>{
+	
+	User findByUsername(String username);
+	User findByEmail(String email);
 
 }
