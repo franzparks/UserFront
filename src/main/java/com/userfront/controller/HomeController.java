604028913afdbed967a2ae06fdd6a8953c941bdf
franzparks <franzparks@gmail.com>
2016-11-04 13:10:05 -0700

    update signup request handlers

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -29,38 +29,37 @@ public class HomeController {
 		return "index";
 	}
 	
-	@RequestMapping(value = "/signup",method = RequestMethod.GET)
-	public void signup(@ModelAttribute("user") User user, Model model){
-		
-		/*if(userService.checkUserExists(user.getUsername(), user.getEmail())){
-			
-			if(userService.checkEmailExists(user.getEmail())){
-				model.addAttribute("emailExists", true);
-			}
-			
-			if(userService.checkUsernameExists(user.getUsername())){
-				model.addAttribute("usernameExists", true);
-			}	
-					
-			return "signup";
-		}else{
-			Set<UserRole> userRoles = new HashSet<>();
-			userRoles.add(new UserRole(user, roleDao.findByName("USER")));
-			userService.createUser(user, userRoles);
-			
-			return "redirect:/";
-		}*/
-	}
-	
-	@RequestMapping(value = "/signup",method = RequestMethod.POST)
-	public String signupPost(Model model){
-		
-		User user = new User();
-		
-		model.addAttribute("user", user);
-		
-		return "signup";
-	}
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
 
+        model.addAttribute("user", user);
+
+        return "signup";
+    }
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signupPost(@ModelAttribute("user") User user,  Model model) {
+
+        if(userService.checkUserExists(user.getUsername(), user.getEmail()))  {
+
+            if (userService.checkEmailExists(user.getEmail())) {
+                model.addAttribute("emailExists", true);
+            }
+
+            if (userService.checkUsernameExists(user.getUsername())) {
+                model.addAttribute("usernameExists", true);
+            }
+
+            return "signup";
+        } else {
+        	 Set<UserRole> userRoles = new HashSet<>();
+             userRoles.add(new UserRole(user, roleDao.findByName("ROLE_USER")));
+
+            userService.createUser(user, userRoles);
+
+            return "redirect:/";
+        }
+    }
 
 }
