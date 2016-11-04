15b832c32153eed67d5e45e7741f16b4aeb20e5f
franzparks <franzparks@gmail.com>
2016-11-04 11:20:02 -0700

    create user dao interface

---
+++ src/main/java/com/userfront/Dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.userfront.Dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.userfront.domain.User;
+
+public interface UserDao extends CrudRepository<User, Long>{
+
+}
