974c6865833a3eec6f1e35ffd596a18c99e3accf
franzparks <franzparks@gmail.com>
2016-11-04 11:29:48 -0700

    autowire user dao

--- src/main/java/com/userfront/service/UserServiceImpl/UserServiceImpl.java
+++ src/main/java/com/userfront/service/UserServiceImpl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.userfront.service.UserServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.userfront.Dao.UserDao;
 import com.userfront.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
-
+	
+	@Autowired
+	private UserDao userDao;
 }
