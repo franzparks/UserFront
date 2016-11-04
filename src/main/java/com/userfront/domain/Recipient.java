24adbe1092b8a5cbe70323315622764291601d65
franzparks <franzparks@gmail.com>
2016-11-03 21:31:38 -0700

    add jpa annotations to recipient class

--- src/main/java/com/userfront/domain/Recipient.java
+++ src/main/java/com/userfront/domain/Recipient.java
@@ -1,9 +1,19 @@
 package com.userfront.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@Entity
 public class Recipient {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
 	private String email;
@@ -11,7 +21,9 @@ public class Recipient {
 	private String accountNumber;
 	private String description;
 	
-	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	@JsonIgnore
 	private User user;
 
 
