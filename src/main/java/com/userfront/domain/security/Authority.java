080a43213925db91be5c449b00a25504c56d6ed3
franzparks <franzparks@gmail.com>
2016-11-05 21:35:30 -0700

    add authority variable

--- src/main/java/com/userfront/domain/security/Authority.java
+++ src/main/java/com/userfront/domain/security/Authority.java
@@ -4,6 +4,8 @@ import org.springframework.security.core.GrantedAuthority;
 
 public class Authority implements GrantedAuthority{
 	
+	private final String authority;
+	
 	public Authority(String authority) {
         this.authority = authority;
     }
