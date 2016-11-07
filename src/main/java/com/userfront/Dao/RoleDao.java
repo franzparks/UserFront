3d4ec560ae9bffddf6d63fdb4d2c3e9b329995af
franzparks <franzparks@gmail.com>
2016-11-06 17:34:47 -0800

    update role dao to interface and extend crud rep

--- src/main/java/com/userfront/Dao/RoleDao.java
+++ src/main/java/com/userfront/Dao/RoleDao.java
@@ -1,5 +1,9 @@
 package com.userfront.Dao;
 
-public class RoleDao {
+import org.springframework.data.repository.CrudRepository;
 
+import com.userfront.domain.security.Role;
+
+public interface RoleDao extends CrudRepository<Role, Integer> {
+	Role findByName(String name);
 }
