dbeeca7b0e91cfcaf00a640e2de39fa82a2b4ec9
franzparks <franzparks@gmail.com>
2016-11-07 19:44:57 -0800

    autowire services

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -1,5 +1,23 @@
 package com.userfront.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.userfront.service.AccountService;
+import com.userfront.service.UserService;
+
+@Controller
+@RequestMapping("/account")
 public class AccountController {
+	
+	@Autowired
+    private UserService userService;
+	
+	@Autowired
+	private AccountService accountService;
+	
+	@Autowired
+	private TransactionService transactionService;
 
 }
