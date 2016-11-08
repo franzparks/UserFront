5674928e2f016116da766e2df5e1b68c58459d05
franzparks <franzparks@gmail.com>
2016-11-07 19:48:54 -0800

    add withdrawPost request handler

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -75,6 +76,13 @@ public class AccountController {
         model.addAttribute("amount", "");
 
         return "withdraw";
+    }
+	
+	@RequestMapping(value = "/withdraw", method = RequestMethod.POST)
+    public String withdrawPOST(@ModelAttribute("amount") String amount, @ModelAttribute("accountType") String accountType, Principal principal) {
+        accountService.withdraw(accountType, Double.parseDouble(amount), principal);
+
+        return "redirect:/userFront";
     }
 
 }
