07c6ccb3975f1c41025b49e47a12488d626b4731
franzparks <franzparks@gmail.com>
2016-11-07 19:46:07 -0800

    add primary account request handler

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -1,9 +1,15 @@
 package com.userfront.controller;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.User;
 import com.userfront.service.AccountService;
 import com.userfront.service.UserService;
 
@@ -19,5 +25,18 @@ public class AccountController {
 	
 	@Autowired
 	private TransactionService transactionService;
+	
+	@RequestMapping("/primaryAccount")
+	public String primaryAccount(Model model, Principal principal) {
+		List<PrimaryTransaction> primaryTransactionList = transactionService.findPrimaryTransactionList(principal.getName());
+		
+		User user = userService.findByUsername(principal.getName());
+        PrimaryAccount primaryAccount = user.getPrimaryAccount();
+
+        model.addAttribute("primaryAccount", primaryAccount);
+        model.addAttribute("primaryTransactionList", primaryTransactionList);
+		
+		return "primaryAccount";
+	}
 
 }
