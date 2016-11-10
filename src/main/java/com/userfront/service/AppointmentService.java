e658bd2f1048908c0fecd0b4ca69a4fd61a80421
franzparks <franzparks@gmail.com>
2016-11-09 17:08:13 -0800

    update appointment service

--- src/main/java/com/userfront/service/AppointmentService.java
+++ src/main/java/com/userfront/service/AppointmentService.java
@@ -1,5 +1,15 @@
 package com.userfront.service;
 
-public class AppointmentService {
+import java.util.List;
 
+import com.userfront.domain.Appointment;
+
+public interface AppointmentService {
+	Appointment createAppointment(Appointment appointment);
+
+    List<Appointment> findAll();
+
+    Appointment findAppointment(Long id);
+
+    void confirmAppointment(Long id);
 }
