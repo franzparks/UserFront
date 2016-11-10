564c4b8ab9623dbd03bb87ab727be8048aa3a069
franzparks <franzparks@gmail.com>
2016-11-10 10:19:41 -0800

    add request handler for confirming and appointment

--- src/main/java/com/userfront/resource/AppointmentResource.java
+++ src/main/java/com/userfront/resource/AppointmentResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,11 @@ public class AppointmentResource {
         List<Appointment> appointmentList = appointmentService.findAll();
 
         return appointmentList;
+    }
+	
+	@RequestMapping("/{id}/confirm")
+    public void confirmAppointment(@PathVariable("id") Long id) {
+        appointmentService.confirmAppointment(id);
     }
 
 
