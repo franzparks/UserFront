d73a6e3003cfd149bdad758b9d38046eccb1b831
franzparks <franzparks@gmail.com>
2016-11-10 10:16:39 -0800

    autowire AppointmentService

--- src/main/java/com/userfront/resource/AppointmentResource.java
+++ src/main/java/com/userfront/resource/AppointmentResource.java
@@ -1,5 +1,12 @@
 package com.userfront.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.userfront.service.AppointmentService;
+
 public class AppointmentResource {
+	
+	@Autowired
+	private AppointmentService appointmentService;
 
 }
