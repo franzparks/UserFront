92de1dc2c149246f84d3f84645bb88bef80feafb
franzparks <franzparks@gmail.com>
2016-11-09 17:14:20 -0800

    update appointment dao

--- src/main/java/com/userfront/Dao/AppointmentDao.java
+++ src/main/java/com/userfront/Dao/AppointmentDao.java
@@ -1,5 +1,12 @@
 package com.userfront.dao;
 
-public interface AppointmentDao {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.userfront.domain.Appointment;
+
+public interface AppointmentDao extends CrudRepository<Appointment, Long> {
+
+    List<Appointment> findAll();
 }
