912520d3831fb212257dfd0d71978d59c296db9a
franzparks <franzparks@gmail.com>
2016-11-10 10:18:13 -0800

    make component rest controller and add mapping

--- src/main/java/com/userfront/resource/AppointmentResource.java
+++ src/main/java/com/userfront/resource/AppointmentResource.java
@@ -2,9 +2,13 @@ package com.userfront.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.userfront.service.AppointmentService;
 
+@RestController
+@RequestMapping("/api/appointment")
 @PreAuthorize("hasRole('ADMIN')")
 public class AppointmentResource {
 	
