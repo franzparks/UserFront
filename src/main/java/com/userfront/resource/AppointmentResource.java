61846df3144853d87d7fd5b214f0621703643616
franzparks <franzparks@gmail.com>
2016-11-10 10:17:28 -0800

    add preuthorizer

--- src/main/java/com/userfront/resource/AppointmentResource.java
+++ src/main/java/com/userfront/resource/AppointmentResource.java
@@ -1,9 +1,11 @@
 package com.userfront.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.userfront.service.AppointmentService;
 
+@PreAuthorize("hasRole('ADMIN')")
 public class AppointmentResource {
 	
 	@Autowired
