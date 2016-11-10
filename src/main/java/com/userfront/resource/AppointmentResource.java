0406c3b67d1a52860b7473f38b4a805280f3c86b
franzparks <franzparks@gmail.com>
2016-11-10 10:19:06 -0800

    add request handler for all appointments

--- src/main/java/com/userfront/resource/AppointmentResource.java
+++ src/main/java/com/userfront/resource/AppointmentResource.java
@@ -1,10 +1,13 @@
 package com.userfront.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.userfront.domain.Appointment;
 import com.userfront.service.AppointmentService;
 
 @RestController
@@ -14,5 +17,13 @@ public class AppointmentResource {
 	
 	@Autowired
 	private AppointmentService appointmentService;
+	
+	@RequestMapping("/all")
+    public List<Appointment> findAppointmentList() {
+        List<Appointment> appointmentList = appointmentService.findAll();
+
+        return appointmentList;
+    }
+
 
 }
