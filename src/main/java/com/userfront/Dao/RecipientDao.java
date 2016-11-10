d5dbe5d03f5dc3dd6752489f63a2073edcdf8d60
franzparks <franzparks@gmail.com>
2016-11-09 16:44:07 -0800

    update recipient dao

--- src/main/java/com/userfront/Dao/RecipientDao.java
+++ src/main/java/com/userfront/Dao/RecipientDao.java
@@ -1,5 +1,15 @@
 package com.userfront.dao;
 
-public class RecipientDao {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.userfront.domain.Recipient;
+
+public interface RecipientDao extends CrudRepository<Recipient, Long> {
+    List<Recipient> findAll();
+
+    Recipient findByName(String recipientName);
+
+    void deleteByName(String recipientName);
 }
