d4954d117f947d752894d64a115bd9b4f805a906
franzparks <franzparks@gmail.com>
2016-11-09 18:40:16 -0800

    update user service impl

--- src/main/java/com/userfront/service/UserServiceImpl/UserServiceImpl.java
+++ src/main/java/com/userfront/service/UserServiceImpl/UserServiceImpl.java
@@ -1,15 +1,14 @@
 package com.userfront.service.UserServiceImpl;
 
-
+import java.util.List;
 import java.util.Set;
-
-import org.springframework.transaction.annotation.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.userfront.dao.RoleDao;
 import com.userfront.dao.UserDao;
@@ -20,60 +19,36 @@ import com.userfront.service.UserService;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
-	
-	@Autowired
-	private RoleDao roleDao;
 	
 	@Autowired
 	private UserDao userDao;
 	
 	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
-	
-	@Autowired
-	private AccountService accountService;
+    private RoleDao roleDao;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private AccountService accountService;
 	
 	public void save(User user) {
         userDao.save(user);
     }
-	
-	public User findByUsername(String username) {
+
+    public User findByUsername(String username) {
         return userDao.findByUsername(username);
     }
-	
-	public User findByEmail(String email) {
+
+    public User findByEmail(String email) {
         return userDao.findByEmail(email);
     }
-	
-	public boolean checkUserExists(String username, String email){
-        if (checkUsernameExists(username) || checkEmailExists(username)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-	
-	public boolean checkUsernameExists(String username) {
-        if (null != findByUsername(username)) {
-            return true;
-        }
-
-        return false;
-    }
-	
-	public boolean checkEmailExists(String email) {
-        if (null != findByEmail(email)) {
-            return true;
-        }
-
-        return false;
-    }
-	
-	
-	public User createUser(User user, Set<UserRole> userRoles) {
+    
+    
+    public User createUser(User user, Set<UserRole> userRoles) {
         User localUser = userDao.findByUsername(user.getUsername());
 
         if (localUser != null) {
@@ -95,5 +70,51 @@ public class UserServiceImpl implements UserService {
         }
 
         return localUser;
+    }
+    
+    public boolean checkUserExists(String username, String email){
+        if (checkUsernameExists(username) || checkEmailExists(username)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkUsernameExists(String username) {
+        if (null != findByUsername(username)) {
+            return true;
+        }
+
+        return false;
+    }
+    
+    public boolean checkEmailExists(String email) {
+        if (null != findByEmail(email)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public User saveUser (User user) {
+        return userDao.save(user);
+    }
+    
+    public List<User> findUserList() {
+        return userDao.findAll();
+    }
+
+    public void enableUser (String username) {
+        User user = findByUsername(username);
+        user.setEnabled(true);
+        userDao.save(user);
+    }
+
+    public void disableUser (String username) {
+        User user = findByUsername(username);
+        user.setEnabled(false);
+        System.out.println(user.isEnabled());
+        userDao.save(user);
+        System.out.println(username + " is disabled.");
     }
 }
