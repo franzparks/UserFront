1597fd3d1924d64121b5e8f38afb1647845ac832
franzparks <franzparks@gmail.com>
2016-11-09 16:02:21 -0800

    update primary transaction dao

--- src/main/java/com/userfront/Dao/PrimaryTransactionDao.java
+++ src/main/java/com/userfront/Dao/PrimaryTransactionDao.java
@@ -1,5 +1,12 @@
 package com.userfront.dao;
 
-public interface PrimaryTransactionDao {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
+import com.userfront.domain.PrimaryTransaction;
+
+public interface PrimaryTransactionDao extends CrudRepository<PrimaryTransaction, Long> {
+
+    List<PrimaryTransaction> findAll();
 }
